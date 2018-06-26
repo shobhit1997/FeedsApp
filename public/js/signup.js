ba6057c68f6ec14d6d0f27e0959c10d30b31db1d
Shobhit Agarwal <shobhit.shobhitagarwal.agarwal@gmail.com>
2018-06-26 19:35:17 +0530

    signup added

---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+jQuery('#signup-form').on('submit',function(e){
+e.preventDefault();
+ var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      var user=JSON.parse(this.responseText);
+      // console.log(window.location.href);
+      var x_auth = xhttp.getResponseHeader('x-auth');
+      localStorage.setItem('_id',user._id);
+      localStorage.setItem('name',user.name);
+      localStorage.setItem('email',user.email);
+      localStorage.setItem('phone',user.phone);
+      localStorage.setItem('x-auth',x_auth);
+
+      window.location.href='/homepage.html';
+    }
+  };
+  xhttp.open("POST", "http://localhost:8000/api/signup", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  var jsonObj={
+    name : jQuery("[name='name']").val(),
+  	phone : jQuery("[name='phone']").val(),
+  	password : jQuery("[name='password']").val(),
+    email   : jQuery("[name='email']").val()
+  };
+  xhttp.send(JSON.stringify(jsonObj));
+});
